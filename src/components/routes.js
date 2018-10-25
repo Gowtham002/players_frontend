475eed99ed12604c14dbcd190b08a71ff8a313d3
Gowtham <gowtham@codingmart.com>
2018-10-25 10:49:25 +0530

    route redirection added

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./home/Home";
 import PlayersHome from "./players/PlayersHome";
 import CreatePlayer from "./players/CreatePlayer";
@@ -9,6 +9,8 @@ const routes = (
     <Route path="/players/new" component={CreatePlayer}/>
     <Route path="/players" component={PlayersHome}/>
     <Route path="/" exact component={Home}/>
+
+    <Redirect to="/" />
   </Switch>
 )
 
